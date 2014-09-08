9bd124210d24a7c119c53b7955b1cf518da7452f
Tony Jian <tonytonyjan@gmail.com>
2014-09-08 20:32:13 +0800

    readable spec

--- spec/jaro_winkler_spec.rb
+++ spec/jaro_winkler_spec.rb
@@ -2,47 +2,65 @@
 require 'jaro_winkler'
 include JaroWinkler
 describe JaroWinkler do
-  before(:all) do
-    @ary = [
-      ['henka', 'henkan', 0.9667],
-      ['al', 'al', 1.0],
-      ['martha', 'marhta', 0.9611],
-      ['jones', 'johnson', 0.8323],
-      ['abcvwxyz', 'cabvwxyz', 0.9583],
-      ['dwayne', 'duane', 0.8400],
-      ['dixon', 'dicksonx', 0.8133],
-      ['fvie', 'ten', 0.0],
-      ['tony', 'tony', 1.0],
-      ['tonytonyjan', 'tonytonyjan', 1.0],
-      ['x', 'x', 1.0],
-      ['', '', 0.0],
-      ['tony', '', 0.0],
-      ['', 'tony', 0.0],
-      ['tonytonyjan', 'tony', 0.8727],
-      ['tony', 'tonytonyjan', 0.8727]
-    ]
-  end
-
   it 'works' do
-    @ary.each do |s1, s2, ans|
+    ary = [
+      ['henka'       , 'henkan'      , 0.9667] ,
+      ['al'          , 'al'          , 1.0]    ,
+      ['martha'      , 'marhta'      , 0.9611] ,
+      ['jones'       , 'johnson'     , 0.8323] ,
+      ['abcvwxyz'    , 'cabvwxyz'    , 0.9583] ,
+      ['dwayne'      , 'duane'       , 0.8400] ,
+      ['dixon'       , 'dicksonx'    , 0.8133] ,
+      ['fvie'        , 'ten'         , 0.0]    ,
+      ['tony'        , 'tony'        , 1.0]    ,
+      ['tonytonyjan' , 'tonytonyjan' , 1.0]    ,
+      ['x'           , 'x'           , 1.0]    ,
+      [''            , ''            , 0.0]    ,
+      ['tony'        , ''            , 0.0]    ,
+      [''            , 'tony'        , 0.0]    ,
+      ['tonytonyjan' , 'tony'        , 0.8727] ,
+      ['tony'        , 'tonytonyjan' , 0.8727]
+    ]
+    ary.each do |s1, s2, ans|
       expect(r_distance(s1, s2)).to be_within(0.0001).of(ans)
       expect(c_distance(s1, s2)).to be_within(0.0001).of(ans)
     end
   end
 
   it 'works with UTF-8' do
-    expect(c_distance('變形金剛4:絕跡重生', '變形金剛4: 絕跡重生')).to eq c_distance('0123456789', '01234x56789')
+    ary = [
+      ['變形金剛4:絕跡重生' , '變形金剛4: 絕跡重生' , 0.9818] ,
+      ['連勝文'             , '連勝丼'              , 0.8222] ,
+      ['馬英九'             , '馬英丸'              , 0.8222] ,
+      ['良い'               , 'いい'                , 0.6666] ,
+    ]
+    ary.each do |s1, s2, ans|
+      expect(r_distance(s1, s2)).to be_within(0.0001).of(ans)
+      expect(c_distance(s1, s2)).to be_within(0.0001).of(ans)
+    end
   end
 
-  it 'can ignore case' do
-    expect(r_distance('MARTHA', 'marhta', case_match: true)).to be_within(0.0001).of(0.9611)
-    expect(c_distance('MARTHA', 'marhta', case_match: true)).to be_within(0.0001).of(0.9611)
+  it 'sets ignore_case' do
+    params = 'MARTHA', 'marhta', {ignore_case: true}
+    expect(r_distance(*params)).to be_within(0.0001).of(0.9611)
+    expect(c_distance(*params)).to be_within(0.0001).of(0.9611)
   end
 
-  it 'can set weight' do
-    expect(r_distance('MARTHA', 'MARHTA', weight: 0.2)).to be_within(0.0001).of(0.9778)
-    expect(c_distance('MARTHA', 'MARHTA', weight: 0.2)).to be_within(0.0001).of(0.9778)
-    expect{ r_distance('MARTHA', 'MARHTA', weight: 0.26) }.to raise_error
-    expect{ c_distance('MARTHA', 'MARHTA', weight: 0.26) }.to raise_error
+  it 'sets weight' do
+    params = 'MARTHA', 'MARHTA', {weight: 0.2}
+    expect(r_distance(*params)).to be_within(0.0001).of(0.9778)
+    expect(c_distance(*params)).to be_within(0.0001).of(0.9778)
+  end
+
+  it 'sets threshold' do
+    params = 'MARTHA', 'MARHTA', {threshold: 0.99}
+    expect(r_distance(*params)).to be_within(0.0001).of(0.9445)
+    expect(c_distance(*params)).to be_within(0.0001).of(0.9445)
+  end
+
+  it 'throws exception when weight exceeding 0.25' do
+    params = 'MARTHA', 'MARHTA', {weight: 0.26}
+    expect{ r_distance(*params) }.to raise_error
+    expect{ c_distance(*params) }.to raise_error
   end
 end
