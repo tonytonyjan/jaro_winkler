c282c8e2ba8da9ba41ff1ad3225caa03a7c7ac4b
Tony Jian <tonytonyjan@gmail.com>
2014-09-08 12:00:20 +0800

    add more benchmark, hotwater and amatch

--- benchmark/native.rb
+++ benchmark/native.rb
@@ -1,6 +1,8 @@
 require 'benchmark'
 require 'jaro_winkler'
 require 'fuzzystringmatch'
+require 'hotwater'
+require 'amatch'
 ary = [['al', 'al'], ['martha', 'marhta'], ['jones', 'johnson'], ['abcvwxyz', 'cabvwxyz'], ['dwayne', 'duane'], ['dixon', 'dicksonx'], ['fvie', 'ten']]
 
 n = 100000
@@ -13,8 +15,18 @@ Benchmark.bmbm do |x|
     jarow = FuzzyStringMatch::JaroWinkler.create(:native)
     n.times{ ary.each{ |str1, str2| jarow.getDistance(str1, str2) } }
   end
+
+  x.report 'hotwater' do
+    n.times{ ary.each{ |str1, str2| Hotwater.jaro_winkler_distance(str1, str2) } }
+  end
+
+  x.report 'amatch' do
+    n.times{ ary.each{ |str1, str2| Amatch::Jaro.new(str1).match(str2) } }
+  end
 end
 
 #                        user     system      total        real
-# jaro_winkler       0.380000   0.000000   0.380000 (  0.386071)
-# fuzzystringmatch   0.140000   0.000000   0.140000 (  0.138053)
+# jaro_winkler       0.420000   0.000000   0.420000 (  0.426742)
+# fuzzystringmatch   0.160000   0.000000   0.160000 (  0.160146)
+# hotwater           0.300000   0.000000   0.300000 (  0.297350)
+# amatch             0.980000   0.010000   0.990000 (  0.982874)
