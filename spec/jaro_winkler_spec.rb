8a9c09c96f90ffcccc3186b62f264e93a20fb336
Tony Jian <tonytonyjan@gmail.com>
2014-09-10 03:59:01 +0800

    enhance spec file

--- spec/jaro_winkler_spec.rb
+++ spec/jaro_winkler_spec.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 require 'jaro_winkler'
 include JaroWinkler
-describe JaroWinkler do
+
+shared_examples 'common' do |strategy|
   it 'works' do
     ary = [
       ['henka'       , 'henkan'      , 0.9667] ,
@@ -22,8 +23,7 @@ describe JaroWinkler do
       ['tony'        , 'tonytonyjan' , 0.8727]
     ]
     ary.each do |s1, s2, ans|
-      expect(r_distance(s1, s2)).to be_within(0.0001).of(ans)
-      expect(c_distance(s1, s2)).to be_within(0.0001).of(ans)
+      expect(send(strategy, s1, s2)).to be_within(0.0001).of(ans)
     end
   end
 
@@ -35,49 +35,50 @@ describe JaroWinkler do
       ['良い'               , 'いい'                , 0.6666] ,
     ]
     ary.each do |s1, s2, ans|
-      expect(r_distance(s1, s2)).to be_within(0.0001).of(ans)
-      expect(c_distance(s1, s2)).to be_within(0.0001).of(ans)
+      expect(send(strategy, s1, s2)).to be_within(0.0001).of(ans)
     end
   end
 
   it 'sets ignore_case' do
-    params = 'MARTHA', 'marhta', {ignore_case: true}
-    expect(r_distance(*params)).to be_within(0.0001).of(0.9611)
-    expect(c_distance(*params)).to be_within(0.0001).of(0.9611)
+    expect(send(strategy, 'MARTHA', 'marhta', ignore_case: true)).to be_within(0.0001).of(0.9611)
   end
 
   it 'sets weight' do
-    params = 'MARTHA', 'MARHTA', {weight: 0.2}
-    expect(r_distance(*params)).to be_within(0.0001).of(0.9778)
-    expect(c_distance(*params)).to be_within(0.0001).of(0.9778)
+    expect(send(strategy, 'MARTHA', 'MARHTA', weight: 0.2)).to be_within(0.0001).of(0.9778)
   end
 
   it 'sets threshold' do
-    params = 'MARTHA', 'MARHTA', {threshold: 0.99}
-    expect(r_distance(*params)).to be_within(0.0001).of(0.9445)
-    expect(c_distance(*params)).to be_within(0.0001).of(0.9445)
+    expect(send(strategy, 'MARTHA', 'MARHTA', threshold: 0.99)).to be_within(0.0001).of(0.9445)
   end
 
-  it 'throws exception when weight exceeding 0.25' do
-    params = 'MARTHA', 'MARHTA', {weight: 0.26}
-    expect{ r_distance(*params) }.to raise_error
-    expect{ c_distance(*params) }.to raise_error
-  end
 
   it 'works with adjusting table' do
     ary = [
       ['HENKA'       , 'HENKAN'      , 0.9667] , # m=5, t=0, s=0
-      ['AL'          , 'AL'          , 1.0]  , # m=2, t=0, s=0
+      ['AL'          , 'AL'          , 1.0   ],  # m=2, t=0, s=0
       ['MARTHA'      , 'MARHTA'      , 0.9611] , # m=6, t=1, s=0
       ['JONES'       , 'JOHNSON'     , 0.8598] , # m=4, t=0, s=3
       ['ABCVWXYZ'    , 'CABVWXYZ'    , 0.9583] , # m=8, t=1, s=0
       ['DWAYNE'      , 'DUANE'       , 0.8730] , # m=4, t=0, s=3
       ['DIXON'       , 'DICKSONX'    , 0.8393] , # m=4, t=0, s=3
-      ['FVIE'        , 'TEN'         , 0.0]
+      ['FVIE'        , 'TEN'         , 0.0   ]
     ]
     ary.each do |s1, s2, ans|
-      expect(r_distance(s1, s2, adj_table: true)).to be_within(0.0001).of(ans)
-      expect(c_distance(s1, s2, adj_table: true)).to be_within(0.0001).of(ans)
+      expect(send(strategy, s1, s2, adj_table: true)).to be_within(0.0001).of(ans)
     end
   end
+
+  context 'with weight exceeding 0.25' do
+    it 'throws exception' do
+      expect{ send(strategy, 'MARTHA', 'MARHTA', weight: 0.26) }.to raise_error
+    end
+  end
+end
+
+describe 'Pure Ruby' do
+  include_examples 'common', :r_distance
+end
+
+describe 'C extention' do
+  include_examples 'common', :c_distance
 end
