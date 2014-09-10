1ec3ec2db87406c85ec69beef930a1db953cdb26
Tony Jian <tonytonyjan@gmail.com>
2014-09-10 17:31:10 +0800

    sturct style

--- ext/jaro_winkler/distance.c
+++ ext/jaro_winkler/distance.c
@@ -3,96 +3,113 @@
 #include <ctype.h>
 #include "distance.h"
 
-Option* option_new(){
-  Option *opt = calloc(1, sizeof(Option));
-  opt->ignore_case = 0;
-  opt->weight = 0.1;
-  opt->threshold = 0.7;
+typedef struct{
+  unsigned long long code;
+  unsigned int byte_length;
+} UnicodeHash;
+
+typedef struct{
+  unsigned long long *ary;
+  int length;
+} Codepoints;
+
+static UnicodeHash unicode_hash_new(const char *str);
+static Codepoints codepoints_new(const char *str, int byte_len);
+// static void build_matrix(const char **adj_table, int length);
+
+Option option_new(){
+  Option opt;
+  opt.ignore_case = 0;
+  opt.weight = 0.1;
+  opt.threshold = 0.7;
   return opt;
 }
 
-void unicode_hash(const char *str, unsigned long long *ret_hash, int *ret_length){
-  unsigned char first_char = str[0];
-  if(first_char >= 252) *ret_length = 6;      // 1111110x
-  else if(first_char >= 248) *ret_length = 5; // 111110xx
-  else if(first_char >= 240) *ret_length = 4; // 11110xxx
-  else if(first_char >= 224) *ret_length = 3; // 1110xxxx
-  else if(first_char >= 192) *ret_length = 2; // 110xxxxx
-  else *ret_length = 1;
-  memcpy(ret_hash, str, *ret_length);
-}
-
-static void codepoints(const char *str, int byte_len, unsigned long long **ret_ary, int *ret_len){
-  *ret_ary = calloc(byte_len, sizeof(long long));
-  int count = 0;
-  for(int i = 0; i < byte_len;){
-    int bytes_num;
-    unicode_hash(str + i, *ret_ary + count, &bytes_num);
-    count++;
-    i += bytes_num;
-  }
-  *ret_len = count;
-}
-
-double c_distance(char *s1, int byte_len1, char *s2, int byte_len2, Option *opt){
+double c_distance(char *s1, int byte_len1, char *s2, int byte_len2, Option opt){
   // set default option if NULL passed
   int free_opt_flag = 0;
-  if(!opt){ free_opt_flag = 1; opt = option_new(); }
 
-  int ary_1_len, ary_2_len;
-  unsigned long long *ary_1, *ary_2;
-  codepoints(s1, byte_len1, &ary_1, &ary_1_len);
-  codepoints(s2, byte_len2, &ary_2, &ary_2_len);
+  Codepoints code_ary_1 = codepoints_new(s1, byte_len1);
+  Codepoints code_ary_2 = codepoints_new(s2, byte_len2);
 
-  if(opt->ignore_case){
-    for(int i = 0; i < ary_1_len; ++i) if(ary_1[i] < 256 && islower(ary_1[i])) ary_1[i] -= 32;
-    for(int i = 0; i < ary_2_len; ++i) if(ary_2[i] < 256 && islower(ary_2[i])) ary_2[i] -= 32;
+  if(opt.ignore_case){
+    for(int i = 0; i < code_ary_1.length; ++i) if(code_ary_1.ary[i] < 256 && islower(code_ary_1.ary[i])) code_ary_1.ary[i] -= 32;
+    for(int i = 0; i < code_ary_2.length; ++i) if(code_ary_2.ary[i] < 256 && islower(code_ary_2.ary[i])) code_ary_2.ary[i] -= 32;
   }
 
   // Guarantee the order
-  if(ary_1_len > ary_2_len){
-    unsigned long long *tmp = ary_1; ary_1 = ary_2; ary_2 = tmp;
-    int tmp2 = ary_1_len; ary_1_len = ary_2_len; ary_2_len = tmp2;
+  if(code_ary_1.length > code_ary_2.length){
+    unsigned long long *tmp = code_ary_1.ary; code_ary_1.ary = code_ary_2.ary; code_ary_2.ary = tmp;
+    int tmp2 = code_ary_1.length; code_ary_1.length = code_ary_2.length; code_ary_2.length = tmp2;
   }
-  int window_size = ary_2_len / 2 - 1;
+  int window_size = code_ary_2.length / 2 - 1;
   if(window_size < 0) window_size = 0;
   double matches     = 0.0;
   int transpositions = 0;
   int previous_index = -1;
-  int max_index      = ary_2_len - 1;
-  for(int i = 0; i < ary_1_len; i++){
+  int max_index      = code_ary_2.length - 1;
+  for(int i = 0; i < code_ary_1.length; i++){
     int left  = i - window_size;
     int right = i + window_size;
     if(left  < 0) left = 0;
     if(right > max_index) right = max_index;
     char matched = 0, found = 0;
     for(int j = left; j <= right; j++){
-      if(ary_1[i] == ary_2[j]){
+      if(code_ary_1.ary[i] == code_ary_2.ary[j]){
         matched = 1;
         if(!found && j > previous_index){
           previous_index = j;
           found = 1;
         }
-      } // if(ary_1[i] == ary_2[j]){
+      } // if(code_ary_1.ary[i] == code_ary_2.ary[j]){
     } // for(int j = left; j <= right; j++){
     if(matched){
       matches++;
       if(!found) transpositions++;
     }
-  } // for(int i = 0; i < ary_1_len; i++){
+  } // for(int i = 0; i < code_ary_1.length; i++){
+
+  // Adjusting table
+  // build_matrix(DEFAULT_ADJ_TABLE, sizeof(DEFAULT_ADJ_TABLE) / 8);
 
   // Don't divide transpositions by 2 since it's been counted directly by above code.
-  double jaro_distance =  matches == 0 ? 0 : (matches / ary_1_len + matches / ary_2_len + (matches - transpositions) / matches) / 3.0;
+  double jaro_distance =  matches == 0 ? 0 : (matches / code_ary_1.length + matches / code_ary_2.length + (matches - transpositions) / matches) / 3.0;
 
   // calculate jaro-winkler distance
-  double threshold = opt->threshold, weight = opt->weight;
+  double threshold = opt.threshold, weight = opt.weight;
   int prefix = 0;
-  int max_length = ary_1_len > 4 ? 4 : ary_1_len;
+  int max_length = code_ary_1.length > 4 ? 4 : code_ary_1.length;
   for(int i = 0; i < max_length; ++i){
-    if(ary_1[i] == ary_2[i]) prefix++;
+    if(code_ary_1.ary[i] == code_ary_2.ary[i]) prefix++;
     else break;
   }
-  free(ary_1); free(ary_2);
-  if(free_opt_flag) free(opt);
+  free(code_ary_1.ary); free(code_ary_2.ary);
   return jaro_distance < threshold ? jaro_distance : jaro_distance + ((prefix * weight) * (1 - jaro_distance));
+}
+
+static UnicodeHash unicode_hash_new(const char *str){
+  UnicodeHash ret;
+  unsigned char first_char = str[0];
+  if(first_char >= 252) ret.byte_length = 6;      // 1111110x
+  else if(first_char >= 248) ret.byte_length = 5; // 111110xx
+  else if(first_char >= 240) ret.byte_length = 4; // 11110xxx
+  else if(first_char >= 224) ret.byte_length = 3; // 1110xxxx
+  else if(first_char >= 192) ret.byte_length = 2; // 110xxxxx
+  else ret.byte_length = 1;
+  memcpy(&ret.code, str, ret.byte_length);
+  return ret;
+}
+
+static Codepoints codepoints_new(const char *str, int byte_len){
+  Codepoints ret = {0};
+  ret.ary = calloc(byte_len, sizeof(long long));
+  int count = 0;
+  for(int i = 0; i < byte_len;){
+    UnicodeHash hash = unicode_hash_new(str + i);
+    ret.ary[count] = hash.code;
+    count++;
+    i += hash.byte_length;
+  }
+  ret.length += count;
+  return ret;
 }
--- ext/jaro_winkler/jaro_winkler.c
+++ ext/jaro_winkler/jaro_winkler.c
@@ -11,17 +11,16 @@ void Init_jaro_winkler(void){
 VALUE rb_distance(int argc, VALUE *argv, VALUE self){
   VALUE s1, s2, opt;
   rb_scan_args(argc, argv, "2:", &s1, &s2, &opt);
-  Option *c_opt = option_new();
+  Option c_opt = option_new();
   if(TYPE(opt) == T_HASH){
     VALUE weight = rb_hash_aref(opt, ID2SYM(rb_intern("weight")));
     VALUE threshold = rb_hash_aref(opt, ID2SYM(rb_intern("threshold")));
     VALUE ignore_case = rb_hash_aref(opt, ID2SYM(rb_intern("ignore_case")));
-    if(!NIL_P(weight)) c_opt->weight = NUM2DBL(weight);
-    if(c_opt->weight > 0.25) rb_raise(rb_eRuntimeError, "Scaling factor should not exceed 0.25, otherwise the distance can become larger than 1.");
-    if(!NIL_P(threshold)) c_opt->threshold = NUM2DBL(threshold);
-    if(!NIL_P(ignore_case)) c_opt->ignore_case = (TYPE(ignore_case) == T_FALSE || NIL_P(ignore_case)) ? 0 : 1;
+    if(!NIL_P(weight)) c_opt.weight = NUM2DBL(weight);
+    if(c_opt.weight > 0.25) rb_raise(rb_eRuntimeError, "Scaling factor should not exceed 0.25, otherwise the distance can become larger than 1.");
+    if(!NIL_P(threshold)) c_opt.threshold = NUM2DBL(threshold);
+    if(!NIL_P(ignore_case)) c_opt.ignore_case = (TYPE(ignore_case) == T_FALSE || NIL_P(ignore_case)) ? 0 : 1;
   }
   VALUE ret = rb_float_new(c_distance(StringValuePtr(s1), RSTRING_LEN(s1), StringValuePtr(s2), RSTRING_LEN(s2), c_opt));
-  free(c_opt);
   return ret;
 }
