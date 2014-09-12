690307cf3b3908fd9e7785410ebff69bad2676a0
Tony Jian <tonytonyjan@gmail.com>
2014-09-12 23:01:08 +0800

    split codepoints part

---
+++ ext/jaro_winkler/codepoints.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
+#include "codepoints.h"
+
+inline UnicodeHash unicode_hash_new(const char *str){
+  UnicodeHash ret = {};
+  unsigned char first_char = str[0];
+  if(first_char >= 252) ret.byte_length = 6;      // 1111110x
+  else if(first_char >= 248) ret.byte_length = 5; // 111110xx
+  else if(first_char >= 240) ret.byte_length = 4; // 11110xxx
+  else if(first_char >= 224) ret.byte_length = 3; // 1110xxxx
+  else if(first_char >= 192) ret.byte_length = 2; // 110xxxxx
+  else ret.byte_length = 1;
+  memcpy(&ret.code, str, ret.byte_length);
+  return ret;
+}
+
+inline Codepoints codepoints_new(const char *str, int byte_len){
+  Codepoints ret = {};
+  ret.ary = malloc(byte_len * sizeof(long long));
+  ret.length = 0;
+  for(int i = 0; i < byte_len;){
+    UnicodeHash hash = unicode_hash_new(str + i);
+    ret.ary[ret.length] = hash.code;
+    ret.length++;
+    i += hash.byte_length;
+  }
+  return ret;
+}
---
+++ ext/jaro_winkler/codepoints.h
@@ -0,0 +1,17 @@
+#ifndef CODEPOINTS_H
+#define CODEPOINTS_H 1
+
+typedef struct{
+  unsigned long long code;
+  unsigned int byte_length;
+} UnicodeHash;
+
+typedef struct{
+  unsigned long long *ary;
+  int length;
+} Codepoints;
+
+UnicodeHash unicode_hash_new(const char *str);
+Codepoints codepoints_new(const char *str, int byte_len);
+
+#endif /* CODEPOINTS_H */
