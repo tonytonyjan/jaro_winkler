791dd5d3f629dbcb943cdf5a0627552d8efe5043
Jian Weihang <tonytonyjan@gmail.com>
2015-06-22 10:44:37 +0800

    separate coding functions to another files. [#9]

--- ext/jaro_winkler/jaro.c
+++ ext/jaro_winkler/jaro.c
@@ -1,30 +1,9 @@
 #include "jaro.h"
+#include "code.h"
 
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-void string_to_codes(char *str, int length, unsigned long long **ret_codes, int *ret_length){
-  unsigned int code;
-  char byte_length;
-
-  *ret_codes = malloc(length * sizeof(long long));
-  *ret_length = 0;
-
-  for(int i = 0; i < length;){
-    unsigned char first_char = (unsigned char)str[i];
-    if(first_char >= 252) byte_length = 6;      // 1111110x
-    else if(first_char >= 248) byte_length = 5; // 111110xx
-    else if(first_char >= 240) byte_length = 4; // 11110xxx
-    else if(first_char >= 224) byte_length = 3; // 1110xxxx
-    else if(first_char >= 192) byte_length = 2; // 110xxxxx
-    else byte_length = 1;
-    (*ret_codes)[*ret_length] = 0;
-    memcpy(&(*ret_codes)[*ret_length], &str[i], byte_length);
-    *ret_length += 1;
-    i += byte_length;
-  }
-}
 
 double jaro_winkler_distance(char* short_str, int short_str_len, char* long_str, int long_str_len, LibJaroOption *opt){
   if(!short_str_len || !long_str_len) return 0.0;
