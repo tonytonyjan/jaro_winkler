72e64cab8a71a1435d4acc9dec1c9811a06cdfb6
Jian Weihang <tonytonyjan@gmail.com>
2015-12-13 01:16:01 +0900

    fix benchmark

--- benchmark/native.rb
+++ benchmark/native.rb
@@ -8,7 +8,7 @@ ary = [['al', 'al'], ['martha', 'marhta'], ['jones', 'johnson'], ['abcvwxyz', 'c
 n = 100000
 Benchmark.bmbm do |x|
   x.report "jaro_winkler #{Gem.loaded_specs['jaro_winkler'].version}" do
-    n.times{ ary.each{ |str1, str2| JaroWinkler.c_distance(str1, str2) } }
+    n.times{ ary.each{ |str1, str2| JaroWinkler.distance(str1, str2) } }
   end
 
   x.report "fuzzystringmatch #{Gem.loaded_specs['fuzzy-string-match'].version}" do
--- benchmark/pure.rb
+++ benchmark/pure.rb
@@ -1,12 +1,12 @@
+require 'jaro_winkler/jaro_winkler_pure'
 require 'benchmark'
-require 'jaro_winkler'
 require 'fuzzystringmatch'
 ary = [['al', 'al'], ['martha', 'marhta'], ['jones', 'johnson'], ['abcvwxyz', 'cabvwxyz'], ['dwayne', 'duane'], ['dixon', 'dicksonx'], ['fvie', 'ten']]
 
 n = 10000
 Benchmark.bmbm do |x|
   x.report "jaro_winkler #{Gem.loaded_specs['jaro_winkler'].version}" do
-    n.times{ ary.each{ |str1, str2| JaroWinkler.r_distance(str1, str2) } }
+    n.times{ ary.each{ |str1, str2| JaroWinkler.distance(str1, str2) } }
   end
 
   x.report "fuzzystringmatch #{Gem.loaded_specs['fuzzy-string-match'].version}" do
