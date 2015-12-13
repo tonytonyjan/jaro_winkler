093bd0910840769aa61ed65b4a911c1128984407
Jian Weihang <tonytonyjan@gmail.com>
2015-12-13 11:22:59 +0900

    there has been gem for java platform since 1.4.0 [ci skip]

--- ext/jaro_winkler/extconf.rb
+++ ext/jaro_winkler/extconf.rb
@@ -1,11 +1,3 @@
-unless RUBY_PLATFORM == 'java'
-  require 'mkmf'
-  $CFLAGS << ' -std=c99 '
-  create_makefile('jaro_winkler/jaro_winkler_ext')
-else
-  dummy_makefile = open('Makefile', 'wb')
-  dummy_makefile.puts '.PHONY: install'
-  dummy_makefile.puts 'install:'
-  dummy_makefile.puts "\t" + '@echo "C extension is not installed, fall back to pure Ruby version instead."'
-  dummy_makefile.close
-end
+require 'mkmf'
+$CFLAGS << ' -std=c99 '
+create_makefile('jaro_winkler/jaro_winkler_ext')
