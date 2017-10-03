5ebd36d3747db7d1b9e88b31eebd7d09b4f2764d
Jian Weihang <tonytonyjan@gmail.com>
2017-10-03 08:06:47 +0800

    chore(benchmark): dry up benchmark codes

--- benchmark/native.rb
+++ benchmark/native.rb
@@ -7,28 +7,23 @@ require 'fuzzystringmatch'
 require 'hotwater'
 require 'amatch'
 
-ary = [
-  %w[al al], %w[martha marhta], %w[jones johnson], %w[abcvwxyz cabvwxyz],
-  %w[dwayne duane], %w[dixon dicksonx], %w[fvie ten]
-]
-
 n = 100_000
 
 Benchmark.bmbm do |x|
   x.report "jaro_winkler (#{`git rev-parse --short HEAD`.chop!})" do
-    n.times { ary.each { |str1, str2| JaroWinkler.distance(str1, str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| JaroWinkler.distance(str1, str2) } }
   end
 
   x.report gem_name_with_version('fuzzy-string-match') do
     jarow = FuzzyStringMatch::JaroWinkler.create(:native)
-    n.times { ary.each { |str1, str2| jarow.getDistance(str1, str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| jarow.getDistance(str1, str2) } }
   end
 
   x.report gem_name_with_version('hotwater') do
-    n.times { ary.each { |str1, str2| Hotwater.jaro_winkler_distance(str1, str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| Hotwater.jaro_winkler_distance(str1, str2) } }
   end
 
   x.report gem_name_with_version('amatch') do
-    n.times { ary.each { |str1, str2| Amatch::Jaro.new(str1).match(str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| Amatch::Jaro.new(str1).match(str2) } }
   end
 end
--- benchmark/pure.rb
+++ benchmark/pure.rb
@@ -5,20 +5,15 @@ require 'benchmark'
 require 'jaro_winkler/jaro_winkler_pure'
 require 'fuzzystringmatch'
 
-ary = [
-  %w[al al], %w[martha marhta], %w[jones johnson], %w[abcvwxyz cabvwxyz],
-  %w[dwayne duane], %w[dixon dicksonx], %w[fvie ten]
-]
-
 n = 10_000
 
 Benchmark.bmbm do |x|
   x.report "jaro_winkler (#{`git rev-parse --short HEAD`.chop!})" do
-    n.times { ary.each { |str1, str2| JaroWinkler.distance(str1, str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| JaroWinkler.distance(str1, str2) } }
   end
 
   x.report gem_name_with_version('fuzzy-string-match') do
     jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
-    n.times { ary.each { |str1, str2| jarow.getDistance(str1, str2) } }
+    n.times { SAMPLES[:ascii].each { |str1, str2| jarow.getDistance(str1, str2) } }
   end
 end
