7e66cbf0055a7c1f1c42744afcf00ed0d580e82d
Jian Weihang <tonytonyjan@gmail.com>
2018-06-07 00:20:13 +0900

    build: build c extension only on MRI platform, and make a dummy Makefile for other platforms
    close #19

--- ext/jaro_winkler/extconf.rb
+++ ext/jaro_winkler/extconf.rb
@@ -1,3 +1,16 @@
-require 'mkmf'
-$CFLAGS << ' -std=c99 '
-create_makefile('jaro_winkler/jaro_winkler_ext')
+# frozen_string_literal: true
+
+if RUBY_ENGINE == 'ruby'
+  require 'mkmf'
+  $CFLAGS << ' -std=c99 '
+  create_makefile('jaro_winkler/jaro_winkler_ext')
+else
+  open('Makefile', 'wb') do |file|
+    file.write <<-MAKEFILE
+.PHONY: install
+install:
+\techo "Cannot compile C Extension, fall back to pure Ruby version."
+clean:
+    MAKEFILE
+  end
+end
