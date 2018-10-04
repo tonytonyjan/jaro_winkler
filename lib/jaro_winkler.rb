fad9196f16186f4fa19a5a9f8497aba2146a45f2
Leonhardt Wille <lwille@users.noreply.github.com>
2018-10-04 13:47:27 +0200

    Allow enforcing pure ruby version
    I was looking for a way to select the pure ruby version. It's unclear to me under which circumstances `RUBY_ENGINE` is not `ruby`.
    
    I'm trying to fix this error, which is due to a nonexistent ruby build environment (and my unwillingness to provide it):
    
    ```
    Fetching jaro_winkler 1.5.1
    Installing jaro_winkler 1.5.1 with native extensions
    Gem::Ext::BuildError: ERROR: Failed to build gem native extension.
    ```

--- lib/jaro_winkler.rb
+++ lib/jaro_winkler.rb
@@ -2,7 +2,7 @@
 
 require 'jaro_winkler/version'
 
-if RUBY_ENGINE == 'ruby'
+if RUBY_ENGINE == 'ruby' && ENV['JARO_WINKLER_PURE_RUBY'].nil?
   require 'jaro_winkler/jaro_winkler_ext'
 else
   require 'jaro_winkler/jaro_winkler_pure'
