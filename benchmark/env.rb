4508599ffa33febc3945271e0782d8bf43bd1c4c
Jian Weihang <tonytonyjan@gmail.com>
2019-01-04 12:13:13 +0800

    chore: fix benchmark

--- benchmark/env.rb
+++ benchmark/env.rb
@@ -3,7 +3,7 @@
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'bundler'
 Bundler.setup(:benchmark)
-require 'samples'
+require File.expand_path('../samples', __FILE__)
 
 def gem_name_with_version(gem)
   "#{gem} (#{Gem.loaded_specs[gem].version})"
